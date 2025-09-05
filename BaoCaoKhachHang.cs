b1d10dd27b77b948f16873a82cd366ad45dc57f4
THANH <anhloc2904@gmail.com>
2025-09-05 10:39:33 +0700

    Sửa file BaoCaoKhachHang.cs

--- BaoCaoKhachHang.cs
+++ BaoCaoKhachHang.cs
@@ -8,7 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+// thanh nguyen da o day 
 namespace ThuongMaiDienTu
 {
     public partial class BaoCaoKhachHang: Form

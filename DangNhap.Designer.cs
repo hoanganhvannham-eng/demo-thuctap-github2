75eb2f90ae2531725add516963285f6ef0b6bd87
KienBeo <cokien173@gmail.com>
2025-09-06 11:00:56 +0700

    sua form dang nhao

--- DangNhap.Designer.cs
+++ DangNhap.Designer.cs
@@ -39,26 +39,29 @@
             // 
             // tbusename
             // 
-            this.tbusename.Location = new System.Drawing.Point(200, 94);
+            this.tbusename.Location = new System.Drawing.Point(150, 76);
+            this.tbusename.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tbusename.Name = "tbusename";
-            this.tbusename.Size = new System.Drawing.Size(309, 22);
+            this.tbusename.Size = new System.Drawing.Size(233, 20);
             this.tbusename.TabIndex = 0;
             // 
             // tbpassword
             // 
             this.tbpassword.AccessibleName = "*";
-            this.tbpassword.Location = new System.Drawing.Point(200, 144);
+            this.tbpassword.Location = new System.Drawing.Point(150, 117);
+            this.tbpassword.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tbpassword.Name = "tbpassword";
             this.tbpassword.PasswordChar = '*';
-            this.tbpassword.Size = new System.Drawing.Size(309, 22);
+            this.tbpassword.Size = new System.Drawing.Size(233, 20);
             this.tbpassword.TabIndex = 1;
             // 
             // btndangnhap
             // 
             this.btndangnhap.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btndangnhap.Location = new System.Drawing.Point(123, 199);
+            this.btndangnhap.Location = new System.Drawing.Point(92, 162);
+            this.btndangnhap.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btndangnhap.Name = "btndangnhap";
-            this.btndangnhap.Size = new System.Drawing.Size(107, 39);
+            this.btndangnhap.Size = new System.Drawing.Size(80, 32);
             this.btndangnhap.TabIndex = 2;
             this.btndangnhap.Text = "Đăng Nhập";
             this.btndangnhap.UseVisualStyleBackColor = true;
@@ -67,9 +70,10 @@
             // btnthoat
             // 
             this.btnthoat.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnthoat.Location = new System.Drawing.Point(367, 199);
+            this.btnthoat.Location = new System.Drawing.Point(275, 162);
+            this.btnthoat.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnthoat.Name = "btnthoat";
-            this.btnthoat.Size = new System.Drawing.Size(100, 39);
+            this.btnthoat.Size = new System.Drawing.Size(75, 32);
             this.btnthoat.TabIndex = 3;
             this.btnthoat.Text = "Thoát";
             this.btnthoat.UseVisualStyleBackColor = true;
@@ -79,20 +83,22 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Times New Roman", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(133, 35);
+            this.label1.Location = new System.Drawing.Point(100, 28);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(463, 37);
+            this.label1.Size = new System.Drawing.Size(253, 31);
             this.label1.TabIndex = 4;
-            this.label1.Text = "khong phai FORM ĐĂNG NHẬP";
+            this.label1.Text = "FORM ĐĂNG NHẬP";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(59, 94);
+            this.label2.Location = new System.Drawing.Point(44, 76);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(135, 22);
+            this.label2.Size = new System.Drawing.Size(105, 19);
             this.label2.TabIndex = 5;
             this.label2.Text = "Tên đăng nhập :";
             // 
@@ -100,17 +106,18 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(59, 144);
+            this.label3.Location = new System.Drawing.Point(44, 117);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(93, 22);
+            this.label3.Size = new System.Drawing.Size(74, 19);
             this.label3.TabIndex = 6;
             this.label3.Text = "Mật khẩu :";
             // 
             // DangNhap
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(594, 394);
+            this.ClientSize = new System.Drawing.Size(446, 320);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -118,6 +125,7 @@
             this.Controls.Add(this.btndangnhap);
             this.Controls.Add(this.tbpassword);
             this.Controls.Add(this.tbusename);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "DangNhap";
             this.Text = "DangNhap";
             this.Load += new System.EventHandler(this.DangNhap_Load);
